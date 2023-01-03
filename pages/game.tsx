ee7a37aff1951506a6a3066fffc0f8c75a5d2b2a
Da-Gu-Gu-Gu <daguguguu@gmail.com>
2023-01-03 14:41:30 +0630

    almost finish ui

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import PlayerCard from "../src/components/PlayerCard";
-import PlayerCardMobile, { PlayerCardMobileRight } from "../src/components/PlayerCardMobile";
+import PlayerCardMobile, {
+  PlayerCardMobileRight,
+} from "../src/components/PlayerCardMobile";
 import Seo from "../src/components/Seo";
 import Slot from "../src/components/Slot";
 import { useEffect } from "react";
-import { menuAnimation, slotAnimation, timerAnimation } from "../src/animations";
+import {
+  menuAnimation,
+  slotAnimation,
+  timerAnimation,
+} from "../src/animations";
 import playerAnimation from "../src/animations/player";
-
 
 export default function Game() {
   const board = [
@@ -18,12 +23,12 @@ export default function Game() {
     [" ", " ", " ", " ", " ", " ", " "],
   ];
 
-  useEffect(()=>{
-    menuAnimation()
-    playerAnimation()
-    slotAnimation()
-    timerAnimation()
-  },[])
+  useEffect(() => {
+    menuAnimation();
+    playerAnimation();
+    slotAnimation();
+    timerAnimation();
+  }, []);
 
   return (
     <>
@@ -31,65 +36,76 @@ export default function Game() {
       <main className="bg-kayan  w-screen  min-h-screen  h-max flex justify-center items-center">
         <div className=" flex w-[100%] md:w-[90%] justify-around items-center p-3 ">
           <div className="lg:block hidden">
-          <PlayerCard name="Player 1" score={20} />
+            <PlayerCard name="Player 1" score={20} />
           </div>
           {/* playground */}
           <div className=" p-3 flex-col justify-center items-center h-screen">
             {/* menu bar */}
             <div className="flex justify-between items-center p-3">
               <Link href={"/"}>
-              <div  className="cursor-pointer menu-btn flex bg-violet-800 py-2 px-5 rounded-lg text-white">
-                {['H','o','m','e'].map(x=>{
-                  return (
-                    <p key={x} className="menu">{x}</p>  
-                  )
-                })
-}       </div>
+                <div className="cursor-pointer menu-btn flex bg-violet-800 py-2 px-5 rounded-lg text-white">
+                  {["H", "o", "m", "e"].map((x) => {
+                    return (
+                      <p key={x} className="menu">
+                        {x}
+                      </p>
+                    );
+                  })}{" "}
+                </div>
               </Link>
               <Link href={"/"}>
                 <div className="cursor-pointer menu-btn flex bg-violet-800 py-2 px-5 rounded-lg text-white">
-                {['R','e','s','t','a','r','t'].map(x=>{
-                  return (
-                    <p key={x} className="menu">{x}</p>  
-                  )
-                })}
+                  {["R", "e", "s", "t", "a", "r", "t"].map((x) => {
+                    return (
+                      <p key={x} className="menu">
+                        {x}
+                      </p>
+                    );
+                  })}
                 </div>
               </Link>
             </div>
 
-            
             <div className="flex justify-between mt-3 mb-5 lg:hidden ">
-            
-            <PlayerCardMobile />
-            <PlayerCardMobileRight />
-
-         
-
+              <PlayerCardMobile score={12}/>
+              <PlayerCardMobileRight score={22} />
             </div>
-{/* game */}
-            <div className=" md:w-[700px]  w-[375px] h-max bg-black pb-3  mt-5 rounded-xl">
-            <div className=" bg-white relative md:w-[700px] w-[375px]  justify-center  pb-14 md:pb-20 border-2 border-black rounded-xl flex flex-wrap">
-              {board.map((x, i) => {
-               return x.map((y, j) => {
-                  return (<Slot key={j.toString()} />)
-                });
-              })}
-
-
-              <div className="flex timer -bottom-[50px] justify-center absolute md:w-[155px] w-[100px] h-[100px] md:h-[155px] md:-bottom-[75px]  bg-black pb-3 rounded-lg clip-part">
-              <div className="clip-part bg-panyaung md:w-[155px] w-[100px]  flex flex-col justify-center items-center  rounded-lg">
-                <p className="md:text-sm text-xs md:mt-0 mt-10">PLAYER 1s TURN</p>
-                <h1 className="font-bold text-3xl text-white">30s</h1>
+            {/* game */}
+            <div className=" md:w-[700px]  w-[375px] h-max pb-3  mt-5 ">
+              <div className="flex justify-center  bg-kayan   ">
+                {[0, 1, 2, 3, 4, 5, 6].map((tab) => {
+                  return (
+                    <div
+                      key={tab}
+                      className="md:w-[70px] opacity-0 hover:opacity-100 cursor-pointer transition-all tab md:h-[70px] p-[2px] pt-[4px] w-[35px] h-[35px] mx-[7px]  md:m-3  bg-black flex justify-center "
+                    >
+                      <div className="md:w-[70px] tab md:h-[70px]  w-[35px] h-[35px] tab bg-panyaung "></div>
+                    </div>
+                  );
+                })}
               </div>
-                
-              </div>
+              <div className="md:w-[700px]  bg-black w-[375px] h-max pb-3  rounded-xl">
+                <div className=" bg-white relative md:w-[700px] w-[375px]  justify-center  pb-14 md:pb-20 border-2 border-black rounded-xl flex flex-wrap">
+                  {board.map((x, i) => {
+                    return x.map((y, j) => {
+                      return <Slot key={j.toString()} />;
+                    });
+                  })}
 
+                  <div className="flex timer -bottom-[50px] justify-center absolute md:w-[155px] w-[100px] h-[100px] md:h-[155px] md:-bottom-[75px]  bg-black pb-3 rounded-lg clip-part">
+                    <div className="clip-part bg-panyaung md:w-[155px] w-[100px]  flex flex-col justify-center items-center  rounded-lg">
+                      <p className="md:text-sm text-xs md:mt-0 mt-10">
+                        PLAYER 1s TURN
+                      </p>
+                      <h1 className="font-bold text-3xl text-white">30s</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-   
           </div>
           <div className="lg:block hidden">
-          <PlayerCard name="Player 2" score={30} isCpu={true} />
+            <PlayerCard name="Player 2" score={30} isCpu={true} />
           </div>
         </div>
       </main>
--- src/components/PlayerCardMobile.tsx
+++ src/components/PlayerCardMobile.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { AiOutlineSmile } from 'react-icons/ai'
 
-const PlayerCardMobile = () => {
+
+interface Props{
+  score:number
+}
+
+const PlayerCardMobile = ({score}:Props) => {
   return (
     <div className="md:w-[200px] player w-[130px] bg-black pb-2 border-2 border-black rounded-xl">
     <div className="md:w-[200px] w-[130px] h-[70px] border-2 border-black flex  j items-center bg-white rounded-xl ">
          <AiOutlineSmile  size={45} className="bg-panyaung rounded-full -ml-5 " />
          <div className="flex md:flex-row flex-col w-full justify-around items-center">
          <p className="font-bold">PLAYER 1</p>
-        <h1 className="font-bold text-3xl">12</h1>
+        <h1 className="font-bold text-3xl">{score}</h1>
         </div>
 </div>
 </div>
@@ -16,14 +21,14 @@ const PlayerCardMobile = () => {
 }
 
 
-export const PlayerCardMobileRight = () => {
+export const PlayerCardMobileRight = ({score}:Props) => {
   return (
     <div className="md:w-[200px] player-right w-[130px] bg-black pb-2 border-2 border-black rounded-xl">
     <div className="md:w-[200px] w-[130px] h-[70px] border-2 border-black flex  j items-center bg-white rounded-xl ">
      
          <div className="flex md:flex-row flex-col-reverse w-full justify-around items-center">
   
-        <h1 className="font-bold text-3xl">21</h1>
+        <h1 className="font-bold text-3xl">{score}</h1>
         <p className="font-bold">PLAYER 2</p>
         </div>
         <AiOutlineSmile  size={45} className="bg-awar rounded-full -mr-5 md:-mr-8 " />
